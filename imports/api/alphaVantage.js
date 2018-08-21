76778369ccf4bd430e8e929c8c822629815fba63
Ameeen Ramadan-Jradi <ameen.ramadan.jradi@gmail.com>
2018-08-22 00:43:44 +1000

    feat(stocks): show message when user has no stocks

--- imports/api/alphaVantage.js
+++ imports/api/alphaVantage.js
@@ -3,16 +3,16 @@ import { Meteor } from 'meteor/meteor';
 const STOCK_QUOTES = "Stock Quotes";
 const baseUrl = 'https://www.alphavantage.co';
 const batchStockQuery = '/query?function=BATCH_STOCK_QUOTES';
-const symbols = '&symbols=AAPL,GOOGL,MSFT,AMZN,FB,JPM,JNJ,XOM,BAC,WFC';
+const symbolsQuery = '&symbols=AAPL,GOOGL,MSFT,AMZN,FB,JPM,JNJ,XOM,BAC,WFC';
 /**
  * ADD YOUR API KEY HERE
  */
 // TODO: Get apikey from environment variables
-const apikey = '&apikey=ADDYOURAPIKEYHERE';
+const apikeyQuery = '&apikey=ADDYOURAPIKEYHERE';
 
 Meteor.methods({
     'stocks.get': function () {
-        const url = baseUrl + batchStockQuery + symbols + apikey;
+        const url = baseUrl + batchStockQuery + symbolsQuery + apikeyQuery;
         let result = HTTP.get(url);
         console.log(result.statusCode);
         if(result.statusCode === 200) {
