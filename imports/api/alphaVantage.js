bdaf4a0c096aeba4a6f55068d8d2515c330e9eca
Ameeen Ramadan-Jradi <ameen.ramadan.jradi@gmail.com>
2018-08-21 23:44:46 +1000

    fix(stocks): add missing $ and alphavantage refactor

--- imports/api/alphaVantage.js
+++ imports/api/alphaVantage.js
@@ -1,24 +1,26 @@
 import { Meteor } from 'meteor/meteor';
 
 const STOCK_QUOTES = "Stock Quotes";
-const url = 'https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&symbols=AAPL,GOOGL,MSFT,AMZN&apikey=BVFDAXSS2YD3KD2O';
+const baseUrl = 'https://www.alphavantage.co';
+const batchStockQuery = '/query?function=BATCH_STOCK_QUOTES';
+const symbols = '&symbols=AAPL,GOOGL,MSFT,AMZN,FB,JPM,JNJ,XOM,BAC,WFC';
+/**
+ * ADD YOUR API KEY HERE
+ */
+// TODO: Get apikey from environment variables
+const apikey = '&apikey=ADDYOURAPIKEYHERE';
 
 Meteor.methods({
     'stocks.get': function () {
+        const url = baseUrl + batchStockQuery + symbols + apikey;
         let result = HTTP.get(url);
-        return result.data[STOCK_QUOTES];
-
-        // HTTP.call('GET', url, (error, result) => {
-        //     if ( error ) throw error;
-        //     console.log("I ran");
-        //     // const jsonResult = JSON.parse(result);
-        //     // console.log(error);
-        //     return result;
-        //     // if (!error) {
-        //     //     return result;
-        //     // } else {
-        //     //     console.log(error);
-        //     // }
-        // });
+        console.log(result.statusCode);
+        if(result.statusCode === 200) {
+            return result.data[STOCK_QUOTES];
+        } else {
+            const errorJson = JSON.parse(result.content);
+            // TODO: Handle error properly
+            throw new Meteor.Error(result.statusCode, errorJson.error);
+        }
     }
 });
